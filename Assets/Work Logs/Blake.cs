1dacfa6bef62048335cdce433a68837171f26a8b
Ghost2600 <90476678+Ghost2600@users.noreply.github.com>
2021-12-01 15:44:13 -0500

    Just added my time

--- Assets/Work Logs/Blake.cs
+++ Assets/Work Logs/Blake.cs
@@ -3,7 +3,8 @@
     
         Work1: added some color and area to the world. #h 25 minutes
         Work2: added a WIP character and some scripts. #h 45 minutes
-        Work3: added ground and movement script. #h 1 hour 
+        Work3: added ground and movement script. #h 1 hour
+        Work4; created coin collct scripts and 2 others #h 2 hours
 
     Total Time: #h
  */
