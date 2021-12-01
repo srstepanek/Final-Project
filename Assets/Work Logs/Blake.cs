efceddcbd4ab00985deb67932902cefa60d9ab05
Ghost2600 <90476678+Ghost2600@users.noreply.github.com>
2021-12-01 14:38:54 -0500

    Added Movement script to PlayerWIP

--- Assets/Work Logs/Blake.cs
+++ Assets/Work Logs/Blake.cs
@@ -1,8 +1,9 @@
 /*
     Blake's Log
     
-        Work1 - #h
-        Work2 - #h
+        Work1: added some color and area to the world. #h 25 minutes
+        Work2: added a WIP character and some scripts. #h 45 minutes
+        Work3: added ground and movement script. #h 1 hour 
 
     Total Time: #h
  */
