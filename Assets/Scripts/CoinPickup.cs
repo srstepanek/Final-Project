26a21790a0b0e30cc82c8d8c9bb17bb11d3162cb
Ghost2600 <90476678+Ghost2600@users.noreply.github.com>
2021-12-01 15:01:33 -0500

    Adding Coins Counter and Scripts

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    { 
+        {
+            if (other.gameObject.CompareTag("Coins"))
+            {
+                Destroy(other.gameObject);
+            }
+        }
+    }
+}
