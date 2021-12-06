b019967df379d209d9925c3ad37dbdf3b5e432cd
Bionic_Scout <roman.higginson@zoominternet.net>
2021-12-06 13:40:58 -0500

    Work Log

--- Assets/Work Logs/Roman.cs
+++ Assets/Work Logs/Roman.cs
@@ -12,7 +12,8 @@
         Added Working Play Button in Upgrade Menu - 1 hour
         Added ALL Upgrade Menu Buttons - 45 minutes
         Made All Upgrade Buttons effect Movement and Coin Mod - 15 minutes
+        Fixed Jump, Added Moving Camera, Fixed Level Loading from menu, added Time pick up - 2 hours 30 minutes
 
 
-        Total Time: 9 hours
+        Total Time: 11.5 hours
  */
