c2ed0f33320ca810e989c82f4d277bd38f7ebfa9
srstepanek <90849999+srstepanek@users.noreply.github.com>
2021-12-05 20:50:07 -0500

    Updated work log

--- Assets/Work Logs/Sam.cs
+++ Assets/Work Logs/Sam.cs
@@ -3,6 +3,8 @@
     
         Main Menu- 1h
         Upgrade Menu - 2h
+        Added button back to main menue 1h
+        Added Endpoint and looped game 2h
 
-    Total Time: #h
+    Total Time: 6h
  */
