f64ee0a8ae8516640e8ab592ec7cb99ae5c470da
Ghost2600 <90476678+Ghost2600@users.noreply.github.com>
2021-12-06 16:37:49 -0500

    Added final house

--- Assets/Work Logs/Blake.cs
+++ Assets/Work Logs/Blake.cs
@@ -4,8 +4,9 @@
         Work1: added some color and area to the world. #h 25 minutes
         Work2: added a WIP character and some scripts. #h 45 minutes
         Work3: added ground and movement script. #h 1 hour
-        Work4; created coin collct scripts and 2 others #h 2 hours
-        Work5; added to the environment and made the level longer #h 1 hour 30 minutes
+        Work4: created coin collct scripts and 2 others #h 2 hours
+        Work5: added to the environment and made the level longer #h 1 hour 30 minutes
+        Work6: Finished enviorment and added final house. h# 1 hour 30 minutes
 
-    Total Time: #h
+    Total Time: #h 7 hours 10 minutes
  */
